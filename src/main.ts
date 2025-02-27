1233a53dbb4b841c136e5b7795aca8c115832272
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-02-27 11:26:49 +0100

    feat: send generics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-function example(value: string): string{
+//com um generic
+// function example<T> (value: T): T {
+//     return value;
+// }
+
+// console.log("String", example<string>("Ok"));
+// console.log("number", example<number>(123));
+// console.log("boolean", example<boolean>(true));
+
+//com um dois array
+
+function example<T, U> (value: T, prm2: U): any {
     return value;
 }
 
-console.log(example("Ok"));
+console.log("string e String", example<string, string>("OK", "string"));
+console.log("number e boolean", example<number, boolean>(123, true));
+console.log("boolean e string", example<boolean, string>(true, "vamos lá"));
